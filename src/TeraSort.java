5218b1db3340b972da5c5ca23ef8310f77a49288
Mikołaj Błaż <mikolajblaz@gmail.com>
2016-11-23 22:56:44 +0100

    Running IntMapper

--- src/TeraSort.java
+++ src/TeraSort.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -8,7 +8,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
@@ -17,29 +17,27 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  */
 
 public class TeraSort {
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+    public static class IntMapper
+            extends Mapper<Text, Text, IntWritable, IntWritable>{
 
         private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        private final static IntWritable two = new IntWritable(2);
+        private final static Random r = new Random();
 
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            IntWritable k = new IntWritable(Integer.parseInt(key.toString()));
+            IntWritable v = new IntWritable(Integer.parseInt(value.toString()));
+
+            IntWritable red = new IntWritable(r.nextInt(10));
+            context.write(red, v);
         }
     }
 
     public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
+            extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
@@ -51,15 +49,23 @@ public class TeraSort {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator",",");
+        // Job
         Job job = Job.getInstance(conf, "TeraSort");
         job.setJarByClass(TeraSort.class);
-        job.setMapperClass(TokenizerMapper.class);
+        // Map-Combine-Reduce
+        job.setMapperClass(IntMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
+        job.setNumReduceTasks(3);
+        // Input
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        KeyValueTextInputFormat.addInputPath(job, new Path(args[0]));
+        // Output
+        job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         boolean status = job.waitForCompletion(true);
         System.exit(status ? 0 : 1);
     }
